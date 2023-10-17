696ff63eda6bd0f2731687b97600c418959933d7
Ashpreet Bedi <ashpreet@phidata.com>
2023-10-17 23:40:05 +0100

    v2.0.18

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -25,11 +25,6 @@ target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", db_url)
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
